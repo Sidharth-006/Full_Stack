07814b4557bd2abe6171537e0fb70d8741d1fae5
Sidharth-006 <sidharthsingh3191@gmail.com>
2026-06-29 13:39:19 +0530

    Fix PostgreSQL SSL configuration for local and production

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,12 +9,18 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+
+    dialectOptions:
+      process.env.NODE_ENV === "production"
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+
+    logging: false,
   }
 );
 
